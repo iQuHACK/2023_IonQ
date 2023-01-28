de119bda11a7851b02ec716006cec4c28f1f9085
Kevin Luo <44309027+kevinzluo@users.noreply.github.com>
2023-01-28 17:21:30 -0500

    working single gate display

--- frontend/mygame.py
+++ frontend/mygame.py
@@ -40,8 +40,9 @@ class QuaganiniGUI():
 
         self.tmp_circuit = QuantumCircuit(5, 5)
         self.tmp_circuit.h(0)
-        self.tmp_circuit.cx(0, 1)
-        self.tmp_circuit.cx(0, 2)
+        self.tmp_circuit.h(1)
+        # self.tmp_circuit.cx(0, 1)
+        # self.tmp_circuit.cx(0, 2)
 
     def on_init(self):
         pygame.init()
@@ -54,11 +55,32 @@ class QuaganiniGUI():
             rect = pygame.Rect(x * self.cell_len, y * self.cell_len, self.cell_len, self.cell_len)
             pygame.draw.rect(self.base_surface, pygame.Color('white'), rect, width = 1)
 
+        self.gate_dict = self.build_gate_surfaces()
         # should setup the item selector area
 
         # do some display stuff
 
+    def build_gate_surfaces(self):
+        # gates: H, X, Z, Controls 
+
+        bounding_rect = pygame.Rect((0, 0), (self.cell_len, self.cell_len))
+
+
+        def build_gen_surface(inner_txt, in_color, back_color):
+            surf = pygame.Surface((self.cell_len, self.cell_len))
+            surf.fill(back_color)
+            txt = self.font.render(inner_txt, True, in_color, back_color)
+            surf.blit(txt, txt.get_rect(center = (0.5 * self.cell_len, 0.5 * self.cell_len)))
+            pygame.draw.rect(surf, pygame.Color("white"), bounding_rect, 3)
+
+            return surf
+        
+        return {"H": build_gen_surface("H", pygame.Color("white"), (0, 0, 255)),
+                "X": build_gen_surface("X", pygame.Color("white"), (0, 255, 0)),
+                "Z": build_gen_surface("Z", pygame.Color("white"), (255, 0, 0)),}
+
     def on_loop(self):
+        # do mouseover handling
         pass
 
     def on_render(self):
@@ -80,7 +102,7 @@ class QuaganiniGUI():
         # draw starting lines
         for i in range(self.n_qubits):
             pygame.draw.line(circ_surf, pygame.Color("white"), (0.5 * self.cell_len, (i + 0.5) * self.cell_len), 
-                                (self.width - 0.5 * self.cell_len, (i + 0.5) * self.cell_len))
+                                (self.width - 0.5 * self.cell_len, (i + 0.5) * self.cell_len), 2)
 
         # draw starting letters
         for i in range(self.n_qubits):
@@ -90,9 +112,21 @@ class QuaganiniGUI():
 
 
         # draw gates
-        
+        for i, circuit_instruction in enumerate(qc._data):
+            self.draw_gate(circ_surf, qc, circuit_instruction, i)
 
         return circ_surf
+
+    def draw_gate(self, circ_surf, qc: QuantumCircuit, circuit_instruction, i):
+        op_name = circuit_instruction.operation.name
+        qreg = qc.qregs[0]
+        if len(op_name) == 1:
+            target = qreg.index(circuit_instruction.qubits[0])
+            surf = self.gate_dict[op_name.upper()]
+            loc = (((2 * i + 2) + 0.5) * self.cell_len, (target + 0.5) * self.cell_len)
+            circ_surf.blit(surf, surf.get_rect(center=loc))
+        else:
+            raise NotImplementedError
 
     def on_event(self, event):
         if event.type == pygame.QUIT:
